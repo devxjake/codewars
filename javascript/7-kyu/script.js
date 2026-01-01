12097ae5fbc841c5804491b6ddd45cef7d4bde92
Jake Samuels <samuels242@googlemail.com>
2026-01-01 22:36:11 +0000

    Isograms

--- javascript/7-kyu/script.js
+++ javascript/7-kyu/script.js
@@ -47,3 +47,15 @@ function descendingOrder(n) {
 
   // return parseInt([...String(n)].sort().reverse().join(""), 10)
 }
+
+// ==== 4. Isograms - https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/javascript ====
+
+/* 
+    An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+    Implement a function that determines whether a string that contains only letters is an isogram. 
+    Assume the empty string is an isogram. Ignore letter case.
+*/
+
+function isIsogram(str) {
+  return new Set(str.toLowerCase()).size === str.length;
+}
